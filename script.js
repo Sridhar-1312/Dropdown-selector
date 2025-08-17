95a3fb3ba3d0f26b2a17f654c25457fac2e5fc78
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-08-17 16:52:08 +0530

    add dropdown

---
+++ script.js
@@ -0,0 +1,27 @@
+var selectfeild = document.getElementById("select-feild");
+var selectText = document.getElementById("selectText");
+var options = document.getElementsByClassName("options");
+var list = document.getElementById("list");
+var arrowIcon = document.getElementById("arrowIcon")
+
+
+selectfeild.onclick = function () {
+    list.classList.toggle("hide")
+    arrowIcon.classList.toggle("rotate")
+
+}
+
+
+
+for (option of options) {
+    option.onclick = function () {
+        selectText.innerHTML = this.textContent; 
+        list.classList.toggle("hide")
+            arrowIcon.classList.toggle("rotate")
+
+
+
+    }
+}
+
+
